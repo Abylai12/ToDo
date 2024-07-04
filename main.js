a422667537cdc409acb2868186d5b6ebcaa19c60
Abylai12 <abilay1208@gmail.com>
2024-07-04 11:36:27 +0800

    duusav

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const outValue2 = document.getElementById("text-input2");
 const outValue3 = document.getElementById("text-input3");
 const outValue4 = document.getElementById("text-input4");
 let taskChangeIndex = -1;
+let todoNum = 0;
 
 // tasks for value
 const taskArray = [];
@@ -34,6 +35,7 @@ function modeChange(changeIndex) {
   taskName.value = taskArray[changeIndex].name;
   taskStatus.value = taskArray[changeIndex].status;
   taskChangeIndex = changeIndex;
+  btnSubmit.classList.add("hide");
 }
 
 btnMode.addEventListener("click", () => {
@@ -52,10 +54,16 @@ function draw() {
   outValue2.innerHTML = "";
   outValue3.innerHTML = "";
   outValue4.innerHTML = "";
+  let todoCount = 0;
+  let inProgressCount = 0;
+  let doneCount = 0;
+  let blockedCount = 0;
 
   for (let i = 0; i < taskArray.length; i++) {
     const newTaskCard = `
-      <div class="d-flex justify-content-between align-items-center border p-2 m-2 ${changecolor()} rounded-4  bg-dark">
+      <div class="d-flex justify-content-between align-items-center nemeh border p-2 m-2  rounded-4   ${changecolor(
+        taskArray[i].status
+      )} bg-dark" >
         <div class="d-flex align-items-center">
         <div class="toirog mx-2"></div>
         <h5 class="fw-lighter fs-5 ms-3 mt-2">
@@ -75,18 +83,22 @@ function draw() {
     switch (taskArray[i].status) {
       case "TODO": {
         outValue1.innerHTML += newTaskCard;
+        todoCount++;
         break;
       }
       case "INPROGRESS": {
         outValue2.innerHTML += newTaskCard;
+        inProgressCount++;
         break;
       }
       case "DONE": {
         outValue3.innerHTML += newTaskCard;
+        doneCount++;
         break;
       }
       case "BLOCKED": {
         outValue4.innerHTML += newTaskCard;
+        blockedCount++;
         break;
       }
       default: {
@@ -94,9 +106,13 @@ function draw() {
       }
     }
   }
+  document.getElementById("todoCount").innerHTML = todoCount;
+  document.getElementById("inProgressCount").innerHTML = inProgressCount;
+  document.getElementById("doneCount").innerHTML = doneCount;
+  document.getElementById("blockedCount").innerHTML = blockedCount;
 }
-function changecolor() {
-  switch (taskStatus.value) {
+function changecolor(status) {
+  switch (status) {
     case "TODO": {
       return "border-danger";
     }
@@ -113,6 +129,6 @@ function changecolor() {
 }
 const deleteTask = (taskIndex) => {
   console.log("Task deleted", taskIndex);
-  taskArray.splice(taskIndex);
+  taskArray.splice(taskIndex, 1);
   draw();
 };
